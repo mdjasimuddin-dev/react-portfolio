3bd0f6c73592ff9038a9b5f94a7b57a67de468d3
Md Jasim Uddin <mjunachole97@gmail.com>
2026-05-10 19:23:57 +0600

    trused seciton added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import HeroSection from "../components/home/HeroSection";
+import StatsSection from "../components/home/StatsSection";
 
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
 
       <HeroSection/>
+      <StatsSection/>
 
 
     </div>
